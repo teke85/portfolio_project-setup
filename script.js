6c4ab1c947eddb487cf47b20d64d768a22113c0c
teke <mutau85@gmail.com>
2022-07-29 10:45:18 +0200

    Add all images to html

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const projects = [
       role: 'Back End Dev',
       year: 2015,
     },
-    screenshot: 'images/desktop-version/project-1.png',
+    screenshot: 'images/works/image_1.png',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release`,
     skills: ['html', 'css', 'javascript'],
     liveLink: 'https://www.canopy.cr/tonic',
@@ -28,7 +28,7 @@ const projects = [
       role: 'Full Stack Dev',
       year: 2015,
     },
-    screenshot: 'images/desktop-version/project-2.png',
+    screenshot: 'images/works/image_2.png',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release`,
     skills: ['html', 'css', 'Ruby on rails', 'javascript'],
     liveLink: 'https://www.facebook.com/',
@@ -42,7 +42,7 @@ const projects = [
       role: 'Full Stack Dev',
       year: 2015,
     },
-    screenshot: 'images/desktop-version/project-3.png',
+    screenshot: 'images/works/image_3.png',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release`,
     skills: ['html', 'css', 'Ruby on rails', 'javascript'],
     liveLink: 'https://www.facebook.com/',
@@ -56,7 +56,7 @@ const projects = [
       role: 'Lead Developer',
       year: 2018,
     },
-    screenshot: 'images/desktop-version/project-4.png',
+    screenshot: 'images/works/image_4.png',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release`,
     skills: ['html', 'css', 'javascript', 'Ruby on rails'],
     liveLink: 'https://www.uber.com/',
@@ -64,154 +64,46 @@ const projects = [
   },
 ];
 
-document.querySelector('.portfolio-card').innerHTML = projects
-  .map(
-    (project) =>
-      `<div class="card-layout">
+const myContent = document.querySelector('.card-container');
+
+const showInHtml = projects.map(
+  (project) =>
+    `<div class="card-layout">
       <div class="image-container">
-        <img
+         <img
           class="image-fluid"
           src=${project.screenshot}
           alt="background_image"
         />
       </div>
-
       <div class="p-title">
         <h3 class="title">${project.title}</h3>
         <ul class="flex-items">
-          <li class="list-item canopy">CANOPY</li>
-          <li class="list-item role">Back End Dev</li>
-          <li class="list-item year">2015</li>
+          <li class="list-item canopy">${project.info.client}</li>
+          <li class="list-item role">${project.info.role}</li>
+          <li class="list-item year">${project.info.year}</li>
         </ul>
         <p class="text_paragraph">
-          A daily selection of privately personalized reads; no accounts or
-          sign-ups required.
+          ${project.description}
         </p>
         <ul class="btn-container-flex">
           <li class="btn">
-            <button class="btn-sm" type="button">html</button>
+            <button class="btn-sm" type="button">${project.skills[0]}</button>
           </li>
           <li class="btn">
-            <button class="btn-md" type="button">css</button>
+            <button class="btn-md" type="button">${project.skills[1]}</button>
           </li>
           <li class="btn">
-            <button class="btn-lg" type="button">javaScript</button>
+            <button class="btn-lg" type="button">${project.skills[2]}</button>
           </li>
         </ul>
         <div class="btn-container">
           <button type="button" class="btn-xxl">See Project</button>
         </div>
-      </div>
-    </div>
-
-    <div class="card-layout">
-      <div class="p-title">
-        <h3 class="title">Tonic</h3>
-        <ul class="flex-items">
-          <li class="list-item canopy">CANOPY</li>
-          <li class="list-item role">Back End Dev</li>
-          <li class="list-item year">2015</li>
-        </ul>
-        <p class="text_paragraph">
-          A daily selection of privately personalized reads; no accounts or
-          sign-ups required.
-        </p>
-        <ul class="btn-container-flex">
-          <li class="btn">
-            <button class="btn-sm" type="button">html</button>
-          </li>
-          <li class="btn">
-            <button class="btn-md" type="button">css</button>
-          </li>
-          <li class="btn">
-            <button class="btn-lg" type="button">javaScript</button>
-          </li>
-        </ul>
-        <div class="btn-container">
-          <button type="button" class="btn-xxl">See Project</button>
-        </div>
-      </div>
-      <div class="image-container">
-        <img
-          class="image-fluid"
-          src="images/works/b.png"
-          alt="background_image"
-        />
-      </div>
-    </div>
-    <div class="card-layout">
-      <div class="image-container">
-        <img
-          class="image-fluid"
-          src="images/works/c.png"
-          alt="background_image"
-        />
-      </div>
-
-      <div class="p-title">
-        <h3 class="title">Tonic</h3>
-        <ul class="flex-items">
-          <li class="list-item canopy">CANOPY</li>
-          <li class="list-item role">Back End Dev</li>
-          <li class="list-item year">2015</li>
-        </ul>
-        <p class="text_paragraph">
-          A daily selection of privately personalized reads; no accounts or
-          sign-ups required.
-        </p>
-        <ul class="btn-container-flex">
-          <li class="btn">
-            <button class="btn-sm" type="button">html</button>
-          </li>
-          <li class="btn">
-            <button class="btn-md" type="button">css</button>
-          </li>
-          <li class="btn">
-            <button class="btn-lg" type="button">javaScript</button>
-          </li>
-        </ul>
-        <div class="btn-container">
-          <button type="button" class="btn-xxl">See Project</button>
-        </div>
-      </div>
-    </div>
-    <div class="card-layout">
-      <div class="p-title">
-        <h3 class="title">Tonic</h3>
-        <ul class="flex-items">
-          <li class="list-item canopy">CANOPY</li>
-          <li class="list-item role">Back End Dev</li>
-          <li class="list-item year">2015</li>
-        </ul>
-        <p class="text_paragraph">
-          A daily selection of privately personalized reads; no accounts or
-          sign-ups required.
-        </p>
-        <ul class="btn-container-flex">
-          <li class="btn">
-            <button class="btn-sm" type="button">html</button>
-          </li>
-          <li class="btn">
-            <button class="btn-md" type="button">css</button>
-          </li>
-          <li class="btn">
-            <button class="btn-lg" type="button">javaScript</button>
-          </li>
-        </ul>
-        <div class="btn-container">
-          <button type="button" class="btn-xxl">See Project</button>
-        </div>
-      </div>
-      <div class="image-container">
-        <img
-          class="image-fluid"
-          src="images/works/d.png"
-          alt="background_image"
-        />
       </div>
     </div>`
-  )
-  .join('');
+);
+myContent.innerHTML = showInHtml;
 
 document.getElementsByClassName('.view-btn').onclick = function () {
   viewProject(this.id);
